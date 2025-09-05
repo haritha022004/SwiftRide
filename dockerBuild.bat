e8e51e89969aac8637ad0c2e8c39a3b8d0620f46
Sai Nikhil <23211A6724@bvrit.ac.in>
2025-09-05 12:33:52 +0530

    Delete all files

--- dockerBuild.bat
+++
@@ -1,3 +0,0 @@
-REM ====== Step 2: Build Docker image ======
-docker build -t %DOCKER_USER%/%IMAGE_NAME%:%BUILD_NUMBER% .
-
--- dockerHubPush.bat
+++
@@ -1,3 +0,0 @@
-REM ====== Step 3: Push to Docker Hub ======
-echo %DOCKER_PASS% | docker login -u %DOCKER_USER% --password-stdin
-docker push %DOCKER_USER%/%IMAGE_NAME%:%BUILD_NUMBER%
