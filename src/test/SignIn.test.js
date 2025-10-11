cfcf0a038d04c4f882ae1097507cff2226558363
viveka-23 <akhilaviveka@gmail.com>
2025-10-11 17:56:16 +0530

    Update SignIn.test.js

--- src/test/SignIn.test.js
+++ src/test/SignIn.test.js
@@ -2,7 +2,7 @@ import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import SignIn from "../pages/SignIn";
 
-// Mock useNavigate from react-router-dom
+
 const mockedNavigate = jest.fn();
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
