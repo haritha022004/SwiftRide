bed274a0f5072c1c99e148a75d78fda64af7856e
viveka-23 <akhilaviveka@gmail.com>
2025-10-11 17:54:46 +0530

    Update App.test.js

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import App from '../app'; // path to your app.jsx
+import App from '../app'; 
 
 describe('App Component', () => {
   test('renders Navbar', () => {
